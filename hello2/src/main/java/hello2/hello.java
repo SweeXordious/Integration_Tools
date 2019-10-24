d7c04fbf26e05ada870c959f4ec7faf3cc4e616d
unknown <ouijdaneelamri@gmail.com>
2019-10-24 09:07:24 +0100

    emma

---
+++ hello2/src/main/java/hello2/hello.java
@@ -0,0 +1,31 @@
+package hello2;
+
+
+public class hello {
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+
+    static class A {
+        private String Test="";
+
+        public A() {
+
+        }
+
+        public void dummy(int nb){
+            nb = 10;
+            if (nb ==0) {
+                System.out.println("This is a Zero");
+            }
+        }
+
+        public int another_Dummy(int nb) {
+            nb = 10;
+            if (nb ==0) {
+                System.out.println("This is a Zero");
+            }
+            return 15;
+        }
+    }
+}
